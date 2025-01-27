ef748b935f1347f9dd563ed0c15f86d6072bb88b
Dmitriy <dima.11031@yandex.ru>
2025-01-27 18:56:23 +0400

    encode

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def decode_from_morse(code: str):
             decode_cod += MorseCodeNu.get(symbol, "")
             decode_cod += MorseCodeEn.get(symbol, "")
     print(decode_cod.capitalize())
+
+
+#  Функция кодирования
+def encode_to_morse(text: str):
+    my_list = []
+    for i in text:
+        my_list.append(MorseCodeRu.get(i))
+        my_list.append(MorseCodeEn.get(i))
+        my_list.append(MorseCodeNu.get(i))
+    print(' '.join([i for i in my_list if i is not None]))
